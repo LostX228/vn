91cd806ab1b3eb884063f0c24675e90c62f0d80a
LostX228 <abdekr40@gmail.com>
2024-09-17 03:11:39 +0100

    Create COD01.js

---
+++ COD01.js
@@ -0,0 +1,13 @@
+
+loopy() 
+ function loopy() { 
+ var sWord ="LostX" 
+ while (sWord != "LostX TM") {// Pass
+ sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋  𝐈𝐎𝐒 ");
+
+ } 
